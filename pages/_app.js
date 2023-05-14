a2a816c0e18faa37fd0237508f0afa15d6940b0f
Agustin Forero <agf48@cornell.edu>
2023-05-14 15:41:34 -0400

    pls fix IG

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,18 @@ import 'bootstrap/dist/css/bootstrap.css'
 import '@/styles/globals.css'
 import { useEffect } from 'react'
 import { Analytics } from '@vercel/analytics/react';
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }) {
 	useEffect(() => {
 		require("bootstrap/dist/css/bootstrap.min.css")
 		require("bootstrap/dist/js/bootstrap.bundle.min.js")
 	}, [])
+
+	// fix annoying fbclid thing that prevents opening from IG
+	const router = useRouter()
+	const fbclid = router.params.fbclid ? router.params.fbclid : ""
+	
 	return (
 		<div className='bg-cool text-white' style={{ minHeight: "100vh", maxWidth: "100vw" }}>
 			<link rel="icon" href="http://www.agforero.com/favicon.ico" />
