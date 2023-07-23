f2a09afd6a58855cecacab95292e04ff495f91db
Agustin Forero <agf48@cornell.edu>
2023-07-22 21:59:10 -0400

    wip

--- components/projects/index/projectEntry.js
+++ components/projects/index/projectEntry.js
@@ -2,6 +2,7 @@ import { LinkSharp } from "react-ionicons"
 import { ReactMarkdown } from "react-markdown/lib/react-markdown"
 
 export default function ProjectEntry({ projectData }) {
+	console.log(projectData)
 	return (
 		<div className={`d-flex flex-row$ w-100 my-5`}>
 			<div className="border border-1 border-sharp flex-grow-1">
@@ -12,7 +13,7 @@ export default function ProjectEntry({ projectData }) {
 							{projectData.org}
 							{' /' + '/ '}
 							{projectData.date}
-							{ projectData.link &&
+							{projectData.link &&
 								<a className="align-top ms-2" href={projectData.link} target="_blank" rel="noreferrer">
 									<LinkSharp cssClasses="mb-1 text-warning" color={""} />
 								</a>
@@ -21,6 +22,18 @@ export default function ProjectEntry({ projectData }) {
 					</div>
 				</div>
 				<div className="px-3">
+					{projectData.video &&
+						<iframe 
+							className="mt-3"
+							width="560" 
+							height="315" 
+							src={`https://www.youtube.com/embed/${projectData.video}`}
+							title="YouTube video player" 
+							frameborder="0" 
+							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+							allowfullscreen
+						/>
+					}
 					<ReactMarkdown
 						linkTarget="_blank"
 						components={{
